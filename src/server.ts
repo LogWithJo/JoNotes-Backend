914e91b3082dc8bffae6aec8d01d95105cc74e97
youssef <ya53114808@gmail.com>
2026-09-08 06:25:17 +0300

    adding port

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,6 @@ app.get("/", (req, res) => {
 	res.status(200).json({ message: "Server is alive!" });
 });
 
-app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
