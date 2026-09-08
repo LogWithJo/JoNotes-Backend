1f7f0a8f893e86fbb821b3f45553342aa543e086
youssef <ya53114808@gmail.com>
2026-09-08 05:49:06 +0300

    adding frontend url

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import notesRoutes from "./routes/notes.routes.js";
 
 const app = express();
 const PORT = 3000;
+
+app.use(
+	cors({
+		origin: "http://localhost:1999", // your frontend's URL during development
+	}),
+);
 
 app.use(express.json());
 app.use("/auth", authRoutes);
