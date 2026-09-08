e4ef906345d19955343669fc8f6f7817639b7519
youssef <ya53114808@gmail.com>
2026-09-08 06:06:20 +0300

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 import notesRoutes from "./routes/notes.routes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
 	cors({
